731b66e5ae417c213fdce98baa5bd15858665602
flylon <yuhsianghuang@ms.aidc.com.tw>
2025-05-26 15:12:10 +0800

    groq

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-HZ-ShOnh8z3VdiJmSe9wjQ", 
-                base_url="http://llm.aidc.com.tw/v1"
+client = OpenAI(api_key="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", 
+                base_url="https://api.groq.com/openai/v1"
                 )
 
 
@@ -12,7 +12,7 @@ def call_llm_api(prompt):
     Call the LLM API with the given prompt.
     """
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="deepseek-r1-distill-llama-70b",
         messages=[
             {"role": "system", "content": "回覆的內容請永遠使用繁體中文"},
             {"role": "user", "content": prompt}
